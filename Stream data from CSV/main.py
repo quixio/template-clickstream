c43c1fec9455bedfc8e42b6f6477b05540b180ee
Jordi Vicedo <jordi@quix.ai>
2023-10-13 13:33:02 +0200

    Make process clearer and add headers to csv

--- Stream data from CSV/main.py
+++ Stream data from CSV/main.py
@@ -1,102 +1,143 @@
+import os
 import quixstreams as qx
 import pandas as pd
 import time
-import os
-from threading import Thread
+from datetime import datetime
+import threading
 
-# should the main loop run?
-run = True
+# True = keep original timings.
+# False = No delay! Speed through it as fast as possible.
+keep_timing = False
 
-# Quix injects credentials automatically to the client.
+# If the process is terminated on the command line or by the container
+# setting this flag to True will tell the loops to stop and the code
+# to exit gracefully.
+shutting_down = False
+
+# Quix Platform injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
 client = qx.QuixStreamingClient()
 
+# The producer topic is where the data will be published to
+# It's the output from this demo data source code.
 print("Opening output topic")
 producer_topic = client.get_topic_producer(os.environ["output"])
 
-# Read the CSV data
-df = pd.read_csv("omniture-logs.tsv", sep="\t")
-date_col_name = 'Unix Timestamp'  # Column name containing the timestamp information
-print("File loaded.")
+# counters for the status messages
+row_counter = 0
+published_total = 0
 
-# Get original col names
-original_cols = list(df.columns)
-original_cols.remove(date_col_name)
-
-# Now let's write this file to some stream in real time.
-# We want to respect the original time deltas, so we'll need some calculations
-# Get the timestamp data in timestamp format
-df['Original_' + date_col_name] = pd.to_datetime(df[date_col_name])
-df['Original_' + date_col_name] = [pd.Timestamp(ti, unit='ns').timestamp() for ti in df['Original_' + date_col_name]]
-df = df.drop(date_col_name, axis=1)
-
-# Let's calculate the original time deltas and then the accumulated timedeltas
-print("Calculating time deltas...")
-df['delta_s_' + date_col_name] = df['Original_' + date_col_name].shift(-1) - df['Original_' + date_col_name]
-df['acc_delta_s_' + date_col_name] = df['delta_s_' + date_col_name].expanding(1).sum()
-
-# Let's generate the new timestamps
-print("Generate new timestamps")
-timestamp_now = pd.Timestamp.now().timestamp()
-df['timestamp'] = (timestamp_now * 1e9) + (df['acc_delta_s_' + date_col_name] * 1e9)
+# how many times you want to loop through the data
+iterations = 10
 
 
-def get_data():
-    global df
+def publish_row(row):
+    global row_counter
+    global published_total
 
-    # Iterate over file
-    while run:
+    # create a DataFrame using the row
+    df_row = pd.DataFrame([row])
 
-        # this will end up loop when all data has been sent
-        if df.empty:
+    # add a new timestamp column with the current data and time
+    df_row['Timestamp'] = datetime.now()
+
+    # publish the data to the Quix stream created earlier
+    stream_producer = producer_topic.get_or_create_stream(row['Visitor Unique ID'])
+    stream_producer.timeseries.publish(df_row)
+
+    row_counter += 1
+
+    if row_counter == 10:
+        row_counter = 0
+        published_total += 10
+        print(f"Published {published_total} rows")
+
+
+def process_csv_file(csv_file):
+    global shutting_down
+    global iterations
+
+    # Read the CSV file into a pandas DataFrame
+    print("TSV file loading.")
+    df = pd.read_csv(csv_file, sep="\t")
+
+    # Get subset of columns
+    df = df[["Unix Timestamp", "Date and Time", "Visitor ID", "Session ID", "Version Information", "Image Request",
+             "Purchase ID", "IP Address", "JavaScript Version", "Cookies Enabled", "Browser Color Depth",
+             "Product Page URL", "Visitor Unique ID"]]
+
+    print("File loaded.")
+
+    row_count = len(df)
+    print(f"Publishing {row_count * iterations} rows.")
+
+    has_timestamp_column = False
+
+    # If the data contains a 'Timestamp'
+    if "Unix Timestamp" in df:
+        has_timestamp_column = True
+        # keep the original timestamp to ensure the original timing is maintained
+        df = df.rename(columns={"Unix Timestamp": "original_timestamp"})
+        print("Timestamp column renamed.")
+
+    # Get the column headers as a list
+    headers = df.columns.tolist()
+
+    # repeat the data 10 times to ensure the replay lasts long enough to
+    # inspect and play with the data
+    for _ in range(0, iterations):
+
+        # If shutdown has been requested, exit the loop.
+        if shutting_down:
             break
 
-        # Use one stream per visitor
-        stream_producer = producer_topic.create_stream(df['UUID'])
+        # Iterate over the rows and send them to the API
+        for index, row in df.iterrows():
 
-        # Get df_to_write
-        filter_df_to_write = (df['timestamp'] <= pd.Timestamp.now().timestamp() * 1e9)
+            # If shutdown has been requested, exit the loop.
+            if shutting_down:
+                break
 
-        # If there are rows to write to the stream at this time
-        if filter_df_to_write.sum() > 0:
+            # Create a dictionary that includes both column headers and row values
+            row_data = {header: row[header] for header in headers}
+            publish_row(row_data)
 
-            # Get the rows to write and write them to the stream
-            df_to_write = df.loc[filter_df_to_write, ['timestamp', 'Original_' + date_col_name] + original_cols]
-            print("Writing {} rows of data".format(len(df_to_write)))
+            if not keep_timing or not has_timestamp_column:
+                # Don't want to keep the original timing or no timestamp? Thats ok, just sleep for 200ms
+                time.sleep(0.2)
+            else:
+                # Delay sending the next row if it exists
+                # The delay is calculated using the original timestamps and ensure the data
+                # is published at a rate similar to the original data rates
+                if index + 1 < len(df):
+                    current_timestamp = pd.to_datetime(row['original_timestamp'], unit='s')
+                    next_timestamp = pd.to_datetime(df.at[index + 1, 'original_timestamp'], unit='s')
+                    time_difference = next_timestamp - current_timestamp
+                    delay_seconds = time_difference.total_seconds()
 
-            stream_producer.timeseries.publish(df_to_write)
-            print(df_to_write.to_string(index=False))
+                    # handle < 0 delays
+                    if delay_seconds < 0:
+                        delay_seconds = 0
 
-            # Update df
-            df = df[filter_df_to_write == False]
-
-        # If there are no rows to write now, wait a bit:
-        else:
-            # Calculate time to the next data point and wait that
-            time_to_wait = df['delta_s_' + date_col_name].iloc[0]
-            print("Waiting for ", time_to_wait, " seconds.")
-            time.sleep(time_to_wait)
+                    time.sleep(delay_seconds)
 
 
+# Run the CSV processing in a thread
+processing_thread = threading.Thread(target=process_csv_file, args=('omniture-logs.tsv',))
+processing_thread.start()
+
+
+# Run this method before shutting down.
+# In this case we set a flag to tell the loops to exit gracefully.
 def before_shutdown():
-    global run
+    global shutting_down
+    print("Shutting down")
 
-    # Stop the main loop
-    run = False
-
-
-def main():
-    thread = Thread(target=get_data)
-    thread.start()
-
-    # handle termination signals and close streams
-    qx.App.run(before_shutdown=before_shutdown)
-
-    # wait for worker thread to end
-    thread.join()
-
-    print("Exiting")
+    # set the flag to True to stop the loops as soon as possible.
+    shutting_down = True
 
 
-if __name__ == "__main__":
-    main()
+# keep the app running and handle termination signals.
+qx.App.run(before_shutdown=before_shutdown)
+
+print("Exiting.")
