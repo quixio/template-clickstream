ddb1432838d15d20a3e4fb77b546df48885e8154
Jordi Vicedo <jordi@quix.ai>
2023-10-17 17:16:08 +0200

    Revert "Remove quix_function and move all logic to main.py"
    This reverts commit cd486579f4f4a0a67b067c18c195ce91278a438d.

--- Enrich With User And Product Data/main.py
+++ Enrich With User And Product Data/main.py
@@ -1,8 +1,8 @@
 import quixstreams as qx
-from datetime import datetime
-import pandas as pd
+from quix_function import QuixFunction
 import os
 import redis
+
 
 # Quix injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
@@ -12,79 +12,21 @@ print("Opening input and output topics")
 consumer_topic = client.get_topic_consumer(os.environ["input"], "default-consumer-group")
 producer_topic = client.get_topic_producer(os.environ["output"])
 
-redis_client = redis.Redis(
+r = redis.Redis(
     host=os.environ['redis_host'],
     port=os.environ['redis_port'],
     password=os.environ['redis_password'],
     decode_responses=True)
 
 
-# Method to calculate the age of a visitor
-def calculate_age(birthdate: str):
-    if birthdate is None:
-        return None
-
-    # Convert the birthdate string to a datetime object
-    birthdate = datetime.strptime(birthdate, '%Y-%m-%d')
-
-    # Get the current date
-    current_date = datetime.now()
-
-    # Calculate the age
-    age = current_date.year - birthdate.year
-
-    # Check if the birthday for this year has already occurred
-    if (current_date.month, current_date.day) < (birthdate.month, birthdate.day):
-        age -= 1
-
-    return age
-
-
-# Method to get the product category for a product from Redis
-def get_product_category(product: str):
-    return redis_client.hget(f'product:{product}', 'cat')
-
-
-# Method to get the visitor gender from Redis
-def get_visitor_gender(visitor: str):
-    # Origin visitor format: {visitor_id}
-    visitor_without_brackets = visitor.strip('{}')
-    return redis_client.hget(f'visitor:{visitor_without_brackets}', 'gender')
-
-
-# Method to get the visitor birthdate from Redis
-def get_visitor_birthdate(visitor: str):
-    visitor_without_brackets = visitor.strip('{}')
-    return redis_client.hget(f'visitor:{visitor_without_brackets}', 'birthday')
-
-
-# Method to get the visitor age
-def get_visitor_age(visitor: str):
-    visitor_without_brackets = visitor.strip('{}')
-    birthday = redis_client.hget(f'visitor:{visitor_without_brackets}', 'birthday')
-    return calculate_age(birthday)
-
-
-# Callback triggered for each new timeseries data. This method will enrich the data
-def on_dataframe_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-    # Enrich data
-    df['Product Category'] = df['Product Page URL'].apply(get_product_category)
-    df['Visitor Gender'] = df['Visitor Unique ID'].apply(get_visitor_gender)
-    df['Visitor Birthdate'] = df['Visitor Unique ID'].apply(get_visitor_birthdate)
-    df['Visitor Age'] = df['Visitor Birthdate'].apply(calculate_age)
-
-    # Create a new stream (or reuse it if it was already created).
-    # We will be using one stream per visitor id, so we can parallelise the processing
-    # because the partitioning key will be the stream id
-    producer_stream = producer_topic.get_or_create_stream(stream_consumer.stream_id)
-    producer_stream.properties.parents.append(stream_consumer.stream_id)
-    producer_stream.timeseries.buffer.publish(df)
-
-
 # Callback called for each incoming stream
 def read_stream(consumer_stream: qx.StreamConsumer):
+    # Handle the data in a function to simplify the example.
+    # Note that we will be creating one function per visitor for this example.
+    quix_function = QuixFunction(consumer_stream, producer_topic, r)
+
     # React to new data received from input topic.
-    consumer_stream.timeseries.on_dataframe_received = on_dataframe_handler
+    consumer_stream.timeseries.on_dataframe_received = quix_function.on_dataframe_handler
 
 
 # Hook up events before initiating read to avoid losing out on any data
---
+++ Enrich With User And Product Data/quix_function.py
@@ -0,0 +1,67 @@
+import quixstreams as qx
+import pandas as pd
+import redis
+from datetime import datetime
+
+
+class QuixFunction:
+    def __init__(self, consumer_stream: qx.StreamConsumer, topic_producer: qx.TopicProducer, r: redis.Redis):
+        self.consumer_stream = consumer_stream
+        self.topic_producer = topic_producer
+        self.redis_client = r
+
+    # Method to calculate the age of a visitor
+    def calculate_age(self, birthdate: str):
+        if birthdate is None:
+            return None
+
+        # Convert the birthdate string to a datetime object
+        birthdate = datetime.strptime(birthdate, '%Y-%m-%d')
+
+        # Get the current date
+        current_date = datetime.now()
+
+        # Calculate the age
+        age = current_date.year - birthdate.year
+
+        # Check if the birthday for this year has already occurred
+        if (current_date.month, current_date.day) < (birthdate.month, birthdate.day):
+            age -= 1
+
+        return age
+
+    # Method to get the product category for a product from Redis
+    def get_product_category(self, product: str):
+        return self.redis_client.hget(f'product:{product}', 'cat')
+
+    # Method to get the visitor gender from Redis
+    def get_visitor_gender(self, visitor: str):
+        # Origin visitor format: {visitor_id}
+        visitor_without_brackets = visitor.strip('{}')
+        return self.redis_client.hget(f'visitor:{visitor_without_brackets}', 'gender')
+
+    # Method to get the visitor birthdate from Redis
+    def get_visitor_birthdate(self, visitor: str):
+        visitor_without_brackets = visitor.strip('{}')
+        return self.redis_client.hget(f'visitor:{visitor_without_brackets}', 'birthday')
+
+    # Method to get the visitor age
+    def get_visitor_age(self, visitor: str):
+        visitor_without_brackets = visitor.strip('{}')
+        birthday = self.redis_client.hget(f'visitor:{visitor_without_brackets}', 'birthday')
+        return self.calculate_age(birthday)
+
+    # Callback triggered for each new timeseries data. This method will enrich the data
+    def on_dataframe_handler(self, stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+        # Enrich data
+        df['Product Category'] = df['Product Page URL'].apply(self.get_product_category)
+        df['Visitor Gender'] = df['Visitor Unique ID'].apply(self.get_visitor_gender)
+        df['Visitor Birthdate'] = df['Visitor Unique ID'].apply(self.get_visitor_birthdate)
+        df['Visitor Age'] = df['Visitor Birthdate'].apply(self.calculate_age)
+
+        # Create a new stream (or reuse it if it was already created).
+        # We will be using one stream per visitor id, so we can parallelise the processing
+        # because the partitioning key will be the stream id
+        producer_stream = self.topic_producer.get_or_create_stream(stream_consumer.stream_id)
+        producer_stream.properties.parents.append(stream_consumer.stream_id)
+        producer_stream.timeseries.buffer.publish(df)
