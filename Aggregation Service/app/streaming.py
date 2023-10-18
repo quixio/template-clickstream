226fd023ab82f410e3ed507759d090f30fb1f7a5
Jordi Vicedo <jordi@quix.ai>
2023-10-18 16:45:30 +0200

    Massage data before appending to state_store

--- Aggregation Service/app/streaming.py
+++ Aggregation Service/app/streaming.py
@@ -5,7 +5,27 @@ from .store import StreamStateStore
 
 __all__ = ("start_quixstreams",)
 
+valid_columns = ["Date and Time", "Product Category", "Visitor Age Group", "Visitor Gender"]
+
 qx.Logging.update_factory(qx.LogLevel.Debug)
+
+def get_age_group(age):
+    if age < 10:
+        return "0-9"
+    elif age < 20:
+        return "10-19"
+    elif age < 30:
+        return "20-29"
+    elif age < 40:
+        return "30-39"
+    elif age < 50:
+        return "40-49"
+    elif age < 60:
+        return "50-59"
+    elif age < 70:
+        return "60-69"
+    else:
+        return "70+"
 
 
 def start_quixstreams(topic_name: str, state_store: StreamStateStore):
@@ -14,7 +34,8 @@ def start_quixstreams(topic_name: str, state_store: StreamStateStore):
     :param topic_name: Input topic name
     :param state_store: Instance of store.StreamStateStore to keep the dataframe rows
     """
-    client = qx.QuixStreamingClient()
+    token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1qVTBRVE01TmtJNVJqSTNOVEpFUlVSRFF6WXdRVFF4TjBSRk56SkNNekpFUWpBNFFqazBSUSJ9.eyJodHRwczovL3F1aXguYWkvb3JnX2lkIjoiZGVtbyIsImh0dHBzOi8vcXVpeC5haS9vd25lcl9pZCI6ImF1dGgwfDExYjRjZmQyLWZiMjctNGIwNS05Y2MzLWRhNmE5YTZlNzRjNyIsImh0dHBzOi8vcXVpeC5haS90b2tlbl9pZCI6ImZmMjJmZDM3LWU3ZDctNDRmOS1iNGVmLTBlYmIwMDJlM2ZkYSIsImh0dHBzOi8vcXVpeC5haS9leHAiOiIxNjk4NzA2ODAwIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLnF1aXguYWkvIiwic3ViIjoiZE1jMjFGVXhsNDJVcE5JSzNHVm5Ka3BtUHpJeGJCa1lAY2xpZW50cyIsImF1ZCI6InF1aXgiLCJpYXQiOjE2OTc2Mzk2ODIsImV4cCI6MTcwMDIzMTY4MiwiYXpwIjoiZE1jMjFGVXhsNDJVcE5JSzNHVm5Ka3BtUHpJeGJCa1kiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMiLCJwZXJtaXNzaW9ucyI6W119.ZXNJTSMtJbVnAQxoaxal2jr0l4QP9mVA_5LgeNQOrfL8YbDTgz5V_5zt6SBR_D79UNs8EvAXm6XLYn8UKdskt99-MkF9pxYJn3_kf1h7isStjeTIRCgO10aItdBV7XYk2BiKcNpQ1Vh6nTuqHOjt5mTv1tLL0VH_pzdoJhOrMkHFDsSSVt8hi7u8JyDJZ0VLLMq78a_tP5dwgpfwDWQKa0brQOlTZVpVS3YC9HNswUj_0Jb0ad6famROk7NBT3lv4hO2ikDybeEZH5qJteQ-fHhnBNaAUkJiGacZTSDzQXM_LyhP3x5wD3DhNE-EQp27uk3allJXorynGbFIeZqeng"
+    client = qx.QuixStreamingClient(token=token)
 
     consumer_topic = client.get_topic_consumer(
         topic_name, None, auto_offset_reset=qx.AutoOffsetReset.Latest
@@ -30,7 +51,10 @@ def start_quixstreams(topic_name: str, state_store: StreamStateStore):
             """
             Callback called for each incoming data frame
             """
-            df_i["datetime"] = pd.to_datetime(df_i["timestamp"])
+            df_i["Date and Time"] = pd.to_datetime(df_i["Date and Time"])
+            df_i["Visitor Age Group"] = df_i["Visitor Age"].apply(get_age_group)
+            df_i = df_i[[col for col in valid_columns if col in df_i.columns]]
+
             # Add new data to the store
             state_store.append(df_i)
 
