2f01f9cb7e62bf3bb881566d09c4044ce746eab1
Jordi Vicedo <jordi@quix.ai>
2023-10-23 13:34:43 +0200

    Enrich with country and device type, needed for the dashboard

--- Enrich With User And Product Data/main.py
+++ Enrich With User And Product Data/main.py
@@ -1,8 +1,11 @@
+import pycountry
 import quixstreams as qx
 from datetime import datetime
 import pandas as pd
 import os
 import redis
+from geoip import geolite2
+from user_agents_next import parse
 
 # Quix injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
@@ -44,6 +47,7 @@ def calculate_age(birthdate: str):
 def get_product_category(product: str):
     return redis_client.hget(f'product:{product}', 'cat') or "Unknown"
 
+
 # Method to get the product title for a product from Redis
 def get_product_title(product: str):
     return redis_client.hget(f'product:{product}', 'title') or "Unknown"
@@ -64,6 +68,36 @@ def get_visitor_age(visitor: str):
     birthday = redis_client.hget(f'visitor:{visitor}', 'birthday')
     return calculate_age(birthday)
 
+
+def get_country_from_ip(ip: str):
+    try:
+        match = geolite2.lookup(ip)
+        if match is not None:
+            country = pycountry.countries.get(alpha_2=match.country)
+            return country.name
+    except Exception as e:
+        print(f"Error looking up country for IP {ip}: {e}")
+
+    return "Unknown"
+
+
+def get_device_type(user_agent: str):
+    try:
+        ua = parse(user_agent)
+        if ua.is_mobile:
+            return "Mobile"
+        elif ua.is_tablet:
+            return "Tablet"
+        elif ua.is_pc:
+            return "Desktop"
+        elif ua.is_bot:
+            return "Bot"
+        else:
+            return "Other"
+    except Exception as e:
+        print(f"Error parsing user agent {user_agent}: {e}")
+
+    return "Unknown"
 
 
 # Callback triggered for each new timeseries data. This method will enrich the data
