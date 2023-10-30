1a9c4f86b2e71c2b8d68f17927f8ef0c5da29d37
Steve Rosam <srosam+quix-demos@gmail.com>
2023-10-30 15:11:00 +0000

    Edited 'run_server.sh'

--- WebShop/run_server.sh
+++ WebShop/run_server.sh
@@ -1,24 +1,3 @@
 #!/bin/sh
-
-# List files and folders in the current working directory
-ls
-
-# Print the contents of environment.prod.ts to the console
-cat ../environments/environment.prod.ts
-
-# Create or update the Angular environment file (environment.prod.ts)
-echo "export const environment = {" > ../environments/environment.prod.ts
-echo "  isProduction: true," >> ../environments/environment.prod.ts
-echo "  TOKEN: '${bearer_token}'," >> ../environments/environment.prod.ts
-echo "  WORKSPACE_ID: '${Quix__Workspace__Id}'," >> ../environments/environment.prod.ts
-echo "  CLICK_TOPIC: '${click_topic}'," >> ../environments/environment.prod.ts
-echo "  OFFERS_TOPIC: '${offers_topic}'," >> ../environments/environment.prod.ts
-echo "  DEPLOYMENT_ID: '${Deployment__Id}'" >> ../environments/environment.prod.ts
-echo "};" >> ../environments/environment.prod.ts
-
-# Print the contents of environment.prod.ts to the console
-cat ../environments/environment.prod.ts
-
-
 # Start the NGINX server
 nginx -g "daemon off;"
