da9c0346efa3e03370264283e2a5e13d0d3f783c
Steve Rosam <srosam+quix-demos@gmail.com>
2023-10-30 14:09:31 +0000

    Edited 'run_server.sh'

--- WebShop/run_server.sh
+++ WebShop/run_server.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # generate .env.prod with required values
-echo "bearer_token=${bearer_token}" > /environments/.env.prod
-echo "Quix__Workspace__Id=${Quix__Workspace__Id}" >> /environments/.env.prod
-echo "Quix__Portal__Api=${Quix__Portal__Api}" >> /environments/.env.prod
-echo "click_topic=${click_topic}" >> /environments/.env.prod
-echo "offers_topic=${offers_topic}" >> /environments/.env.prod
+echo "bearer_token=${bearer_token}" > /environments/.environment.prod.ts
+echo "Quix__Workspace__Id=${Quix__Workspace__Id}" >> /environments/.environment.prod.ts
+echo "Quix__Portal__Api=${Quix__Portal__Api}" >> /environments/.environment.prod.ts
+echo "click_topic=${click_topic}" >> /environments/.environment.prod.ts
+echo "offers_topic=${offers_topic}" >> /environments/.environment.prod.ts
 nginx -g "daemon off;"
