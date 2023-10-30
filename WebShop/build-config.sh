e89bea5e007139a3904f0bc400ebe84b43de9f48
Steve Rosam <srosam+quix-demos@gmail.com>
2023-10-30 15:28:25 +0000

    Edited 'build-config.sh'

--- WebShop/build-config.sh
+++ WebShop/build-config.sh
@@ -2,17 +2,17 @@
 ls
 
 # Print the contents of environment.prod.ts to the console
-cat /src/environments/environment.prod.ts
+cat /app/src/environments/environment.prod.ts
 
 # Create or update the Angular environment file (environment.prod.ts)
-echo "export const environment = {" > /src/environments/environment.prod.ts
-echo "  isProduction: true," >> /src/environments/environment.prod.ts
-echo "  TOKEN: '${bearer_token}'," >> /src/environments/environment.prod.ts
-echo "  WORKSPACE_ID: '${Quix__Workspace__Id}'," >> /src/environments/environment.prod.ts
-echo "  CLICK_TOPIC: '${click_topic}'," >> /src/environments/environment.prod.ts
-echo "  OFFERS_TOPIC: '${offers_topic}'," >> /src/environments/environment.prod.ts
-echo "  DEPLOYMENT_ID: '${Deployment__Id}'" >> /src/environments/environment.prod.ts
-echo "};" >> /src/environments/environment.prod.ts
+echo "export const environment = {" > /app/src/environments/environment.prod.ts
+echo "  isProduction: true," >> /app/src/environments/environment.prod.ts
+echo "  TOKEN: '${bearer_token}'," >> /app/src/environments/environment.prod.ts
+echo "  WORKSPACE_ID: '${Quix__Workspace__Id}'," >> /app/src/environments/environment.prod.ts
+echo "  CLICK_TOPIC: '${click_topic}'," >> /app/src/environments/environment.prod.ts
+echo "  OFFERS_TOPIC: '${offers_topic}'," >> /app/src/environments/environment.prod.ts
+echo "  DEPLOYMENT_ID: '${Deployment__Id}'" >> /app/src/environments/environment.prod.ts
+echo "};" >> /app/src/environments/environment.prod.ts
 
 # Print the contents of environment.prod.ts to the console
-cat /src/environments/environment.prod.ts
+cat /app/src/environments/environment.prod.ts
