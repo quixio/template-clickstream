0ca0f36bf94a6ed435e611a3f5d47e69c97a23cf
Steve Rosam <srosam+quix-demos@gmail.com>
2023-10-30 14:13:09 +0000

    Edited 'run_server.sh'

--- WebShop/run_server.sh
+++ WebShop/run_server.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-# generate .environment.prod.ts with required values
-echo "bearer_token=${bearer_token}" > /environments/.environment.prod.ts
-echo "Quix__Workspace__Id=${Quix__Workspace__Id}" >> /environments/.environment.prod.ts
-echo "Quix__Portal__Api=${Quix__Portal__Api}" >> /environments/.environment.prod.ts
-echo "click_topic=${click_topic}" >> /environments/.environment.prod.ts
-echo "offers_topic=${offers_topic}" >> /environments/.environment.prod.ts
+
+# Create or update the Angular environment file (environment.prod.ts)
+echo "export const environment = {" > /usr/share/nginx/html/environment.prod.ts
+echo "  isProduction: true," >> /usr/share/nginx/html/environment.prod.ts
+echo "  TOKEN: '${bearer_token}'," >> /usr/share/nginx/html/environment.prod.ts
+echo "  WORKSPACE_ID: '${Quix__Workspace__Id}'," >> /usr/share/nginx/html/environment.prod.ts
+echo "  CLICK_TOPIC: '${click_topic}'," >> /usr/share/nginx/html/environment.prod.ts
+echo "  OFFERS_TOPIC: '${offers_topic}'," >> /usr/share/nginx/html/environment.prod.ts
+echo "  DEPLOYMENT_ID: '${Deployment__Id}'" >> /usr/share/nginx/html/environment.prod.ts
+echo "};" >> /usr/share/nginx/html/environment.prod.ts
+
+# Start the NGINX server
 nginx -g "daemon off;"
