54b9b35175aba0999bba64a9cbac4f1f2328200b
Steve Rosam <srosam+quix-demos@gmail.com>
2023-10-30 14:09:38 +0000

    Edited 'run_server.sh'

--- WebShop/run_server.sh
+++ WebShop/run_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# generate .env.prod with required values
+# generate .environment.prod.ts with required values
 echo "bearer_token=${bearer_token}" > /environments/.environment.prod.ts
 echo "Quix__Workspace__Id=${Quix__Workspace__Id}" >> /environments/.environment.prod.ts
 echo "Quix__Portal__Api=${Quix__Portal__Api}" >> /environments/.environment.prod.ts
