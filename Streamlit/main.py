f6537eedca063359bd580264ee5854aa9c885019
Steve Rosam <srosam+quix-demos@gmail.com>
2023-11-06 15:58:30 +0000

    Edited 'main.py'

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -27,9 +27,18 @@ redis_password = ""
 # check for Quix__Workspace__Id to see if we are in Quix platform.
 if os.environ.get("Quix__Workspace__Id") is not None:
     # this is running in Quix
+    # attempt to get these from environment variables
+    redis_host = os.environ.get("redis_host")
+    redis_port = os.environ.get("redis_port")
+    redis_password = os.environ.get("redis_password")
     pass
 elif st.secrets["redis_host"] is not None:
     # this is running in Streamlit and the 'redis_host' secret is available
+    # attempt to get these from streamlit secrets
+    redis_host = st.secrets.redis_host
+    redis_port = st.secrets.redis_port
+    redis_password = st.secrets.redis_password
+
     pass
 else:
     # we don't know where this is running. Make sure you set the values for:
