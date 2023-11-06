b845cf6c9a2f539cbc12cd1d6d86e92ac032c0a1
Jordi Quix <jordi+demo@quix.io>
2023-11-06 08:24:09 +0000

    Edited 'main.py'

--- Aggregation Service/main.py
+++ Aggregation Service/main.py
@@ -151,6 +151,9 @@ def aggregate_eight_hours(df: pd.DataFrame):
     if not eight_hours.equals(eight_hours_aggregation):
         db["eight_hours_aggregation"] = eight_hours
 
+    # Group by datetime and sum the count
+    eight_hours = eight_hours.groupby(['datetime']).sum().reset_index()
+
     # Store the aggregated_df in Redis
     r.set("sessions", eight_hours.to_json())
 
