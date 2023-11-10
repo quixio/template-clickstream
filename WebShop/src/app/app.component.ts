b3a5b7b1c6f068fef1f41f5728c7bec490b1e884
Steve <100689438+SteveRosam@users.noreply.github.com>
2023-11-10 14:42:48 +0000

    Generate user id. Add age and gender fields

--- WebShop/src/app/app.component.ts
+++ WebShop/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { USERS } from './constants/users';
 import { User } from './models/user';
 import { DataService } from './services/data.service';
 import { ConnectionStatus, QuixService } from './services/quix.service';
 import { MediaObserver } from '@angular/flex-layout';
 import { FormControl } from '@angular/forms';
 import { EventData } from './models/eventData';
+import { sixLetterWordList, threeLetterWordList} from './constants/words'
 
 @Component({
   selector: 'app-root',
@@ -14,15 +14,31 @@ import { EventData } from './models/eventData';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  users: User[] = USERS;
-  userControl = new FormControl();
+
+  ages = Array.from({length: 48}, (_, i) => i + 18);
+  ageControl = new FormControl(this.ages[0]);
+
+  genders = ["Female", "Male", "Unknown"]
+  genderControl = new FormControl(this.genders[0]);
+
   workspaceId: string;
   deploymentId: string;
   ungatedToken: string;
-
+  userId: string;
+  user: User;
   constructor(private quixService: QuixService, private dataService: DataService, public media: MediaObserver) {}
 
   ngOnInit(): void {
+    this.user = {
+      userId: this.generateUniqueWords(),
+      gender: this.genderControl.value || "Female",
+      age: this.ageControl.value || 18
+    };
+
+    this.dataService.user = this.user;
+    const topicId = this.quixService.workspaceId + '-' + this.quixService.offersTopic;
+    this.quixService.subscribeToEvent(topicId, this.userId, "offer");
+
     this.workspaceId = this.quixService.workspaceId;
     this.ungatedToken = this.quixService.ungatedToken;
     this.deploymentId = environment.DEPLOYMENT_ID || '';
@@ -33,18 +49,42 @@ export class AppComponent implements OnInit {
 
     this.quixService.readerConnStatusChanged$.subscribe((status) => {
       if (status !== ConnectionStatus.Connected) return;
-      this.userControl.setValue(this.dataService.user || USERS[0])
+      this.ageControl.setValue(this.dataService.user.age)
     });
 
-    this.userControl.valueChanges.subscribe((user: User) => {
-      const topicId = this.quixService.workspaceId + '-' + this.quixService.offersTopic;
-      if (this.dataService.user) this.quixService.unsubscribeFromEvent(topicId, this.dataService.user.userId, "offer");
-      this.quixService.subscribeToEvent(topicId, user.userId, "offer");
-      this.dataService.user = user;
+    this.ageControl.valueChanges.subscribe((age) => {
+      this.user.age = age || 18;
+    });
+
+    this.genderControl.valueChanges.subscribe((gender) => {
+      this.user.gender = gender || "Female";
     });
   }
 
   toggleSidenav(isOpen: boolean): void {
     this.dataService.isSidenavOpen$.next(isOpen);
+  }
+
+  generateUniqueWords(): string {
+    const threeLetterWords: string[] = [];
+    const sixLetterWords: string[] = [];
+
+    while (threeLetterWords.length < 2) {
+      const randomIndex = Math.floor(Math.random() * threeLetterWordList.length);
+      const randomWord = threeLetterWordList[randomIndex];
+      if (!threeLetterWords.includes(randomWord)) {
+        threeLetterWords.push(randomWord);
+      }
+    }
+
+    while (sixLetterWords.length < 1) {
+      const randomIndex = Math.floor(Math.random() * sixLetterWordList.length);
+      const randomWord = sixLetterWordList[randomIndex];
+      if (!sixLetterWords.includes(randomWord)) {
+        sixLetterWords.push(randomWord);
+      }
+    }
+
+    return `${threeLetterWords[0]}-${sixLetterWords[0]}-${threeLetterWords[1]}`;
   }
 }
--- WebShop/src/app/components/dialog/dialog.component.ts
+++ WebShop/src/app/components/dialog/dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { USERS } from 'src/app/constants/users';
 import { EventData } from 'src/app/models/eventData';
 import { User } from 'src/app/models/user';
 
