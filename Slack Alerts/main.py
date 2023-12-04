4d304c3ee4e28ceabe4295d7f3825b1d975fd700
Jordi Vicedo <jordi@quix.ai>
2023-12-04 16:18:38 +0100

    Implement alerts

--- Slack Alerts/main.py
+++ Slack Alerts/main.py
@@ -1,33 +1,99 @@
+import asyncio
+from datetime import datetime, timedelta
+
 import quixstreams as qx
 from quix_function import QuixFunction
 import os
+import pandas as pd
+import requests
+
+last_timestamp = None
+last_message_sent = None
+lock = asyncio.Lock()
+
+# Send alerts to this Slack webhook URL
+webhook_url = os.environ.get("webhook_url")
+
+# Send alerts if we haven't received data in the last hour
+timeout = int(os.environ.get("timeout", 3600))
+
+# Send alerts every 10 minutes
+interval = int(os.environ.get("interval", 600))
+
+# keep the main loop running
+run = True
 
 
-# Quix injects credentials automatically to the client.
-# Alternatively, you can always pass an SDK token manually as an argument.
-client = qx.QuixStreamingClient()
+def on_dataframe_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+    global last_timestamp, lock
 
-print("Opening input and output topics")
-consumer_topic = client.get_topic_consumer(os.environ["input"], "default-consumer-group-5")
+    async with lock:
+        last_timestamp = datetime.now()
 
-webhook_url = os.environ["webhook_url"]
+    # Here we can inspect the data and send alerts if needed, for example if category is None
+    if df["category"].isnull().values.any():
+        print("No category found in data. Sending alert message.")
+        send_alert_message(f"No category found. Please, check the Data Enrichment service.")
+
 
 # Callback called for each incoming stream
 def read_stream(stream_consumer: qx.StreamConsumer):
-
-    # Create a new stream to output data
-    # handle the data in a function to simplify the example
-    quix_function = QuixFunction(webhook_url, stream_consumer)
-        
     # React to new data received from input topic.
-    stream_consumer.timeseries.on_dataframe_received = quix_function.on_dataframe_handler
-    stream_consumer.events.on_data_received = quix_function.on_event_data_handler
+    stream_consumer.timeseries.on_dataframe_received = on_dataframe_handler
 
-# Hook up events before initiating read to avoid losing out on any data
-consumer_topic.on_stream_received = read_stream
 
-# Hook up to termination signal (for docker image) and CTRL-C
-print("Listening to streams. Press CTRL-C to exit.")
+def send_alert_message(msg: str):
+    global last_message_sent, webhook_url
 
-# Handle graceful exit of the model.
-qx.App.run()
+    if last_message_sent is None or last_message_sent < datetime.now() - timedelta(seconds=interval):
+        last_message_sent = datetime.now()
+        slack_message = {"message": msg}
+        res = requests.post(webhook_url, json=slack_message)
+        if not res.ok:
+            print(res.status_code, res.text)
+    else:
+        print(f"Alert message was sent less than {interval} seconds ago. Skipping.")
+
+
+# Check that we have received data in the last 1 hour
+async def check_data():
+    global last_timestamp, lock
+
+    while run:
+        async with lock:
+            if last_timestamp is not None and last_timestamp < datetime.now() - timedelta(
+                    seconds=timeout) and webhook_url is not None:
+                # Send alert message
+                print("No data received in the last hour. Sending alert message.")
+                send_alert_message(f"No data received in the last hour in topic {os.environ['input']}")
+
+        await asyncio.sleep(60)
+
+
+def before_shutdown():
+    global run
+    run = False
+
+
+async def start_loop():
+    # Quix injects credentials automatically to the client.
+    # Alternatively, you can always pass an SDK token manually as an argument.
+    client = qx.QuixStreamingClient()
+
+    print("Opening input and output topics")
+    consumer_topic = client.get_topic_consumer(os.environ["input"])
+
+    # Hook up events before initiating read to avoid losing out on any data
+    consumer_topic.on_stream_received = read_stream
+
+    print("Listening to streams. Press CTRL-C to exit.")
+    qx.App.run(before_shutdown=before_shutdown)
+
+
+async def main():
+    # Start the main loop
+    await asyncio.gather(start_loop(), check_data())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
