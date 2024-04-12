d1fc1b473dd4efdd3b9441a3b09036e808b030f3
steverosam <steve@quix.ai>
2024-04-12 12:10:52 +0100

    update last 10 visitors

--- last_10_visitors/main.py
+++ last_10_visitors/main.py
@@ -1,15 +1,14 @@
 from quixstreams import Application
-from quixstreams.context import message_context
-from datetime import timedelta, datetime
+from quixstreams.state import State
 import os
 
 # import the dotenv module to load environment variables from a file
 from dotenv import load_dotenv
 load_dotenv()
 
-
+import uuid
 def main():
-    app = Application(consumer_group="site-visitor-details", use_changelog_topics=False)
+    app = Application(consumer_group="site-visitor-details"+str(uuid.uuid4()), auto_offset_reset='earliest', use_changelog_topics=False)
 
     # Define the topic using the "output" environment variable
     input_topic_name = os.getenv("input", "")
@@ -27,50 +26,28 @@ def main():
     # Create a StreamingDataFrame to process inbound data
     sdf = app.dataframe(input_topic)
 
-    # this function will be called for all but the first message
-    # it increments the stateful count of the product
-     # this funciton will be called for every row received
-    # it adds the product from the row to state
-    def reducer(aggregated: dict, row: dict):
-        # Check if 'userId' is in any of the dictionaries in the array
-        if not any(d['userId'] == row['userId'] for d in aggregated):
-            # If the userId is not found, add the new dictionary to the array
-            aggregated.append({
-                "userId": row["userId"],
-                "ip": row["ip"],
-                "country": row["country"],
-                "timestamp": message_context().timestamp.milliseconds * 1000 * 1000,
-            })
-        
-        return aggregated
+    def get_users(message, state: State):
+        users = state.get('users', {}) # get the users dict from state, or init to empty dict
+        userId = message['userId']
+        ip = message['ip']
+        country = message['country']
 
+        if userId not in users: # if we havent seen this user
+            users[userId] = {'ip': ip, 'country': country}
 
-    # this function will be called for only the first message
-    # it initializes the state object with the productId count (starting with 1)
-    def initializer(row: dict):
-        return [{
-                "userId": row["userId"],
-                "ip": row["ip"],
-                "country": row["country"],
-                "timestamp": message_context().timestamp.milliseconds * 1000 * 1000,
-            }]
-        
-    # create a hopping window
-    # pass each row to the reducer/init functions
-    sdf = sdf.hopping_window(timedelta(seconds=60), timedelta(seconds=30)).reduce(reducer, initializer).current()
+            # Ensure only the last 10 items are kept
+            if len(users) > 10:
+                oldest_keys = list(users)[:-10]  # Get all keys except the last 10
+                for key in oldest_keys:
+                    del users[key]  # Remove the oldest entries
+                    
+            state.set('users', users) # update state with the latest list
+        return users # return this dict, it's what we want to see in the downstream service
 
-    def last_10_in_window(rows: dict):
-        # Sort the dictionary by time and take the most recent 10
-        return sorted(rows, key=lambda x: x['timestamp'], reverse=True)[:10]
-        
-    # now we overwrite the 'value' column with:
-    # the top 10 most recent users
-    sdf["value"] = sdf["value"].apply(last_10_in_window)
+    # apply this function to each message received from kafka, enable state.
+    sdf = sdf.apply(get_users, True)
 
-    sdf = sdf.apply(lambda row: row['value'][0])
-
-    # print data after any stage of the pipeline to see what you're working with
-    sdf = sdf.update(lambda row: print(row))
+    sdf = sdf.update(lambda row: print(f'{row}'))
 
     # publish the data to a topic
     sdf = sdf.to_topic(output_topic)    
