9d5a5073326fcbfba117f95239e2aa0e304cb68a
steverosam <steve@quix.ai>
2024-04-15 07:49:31 +0100

    update visitors last 15 mins

--- visitors_15min/main.py
+++ visitors_15min/main.py
@@ -1,5 +1,5 @@
 from quixstreams import Application
-from datetime import timedelta
+from datetime import timedelta, datetime
 import os
 
 # import the dotenv module to load environment variables from a file
@@ -7,8 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+import uuid
 def main():
-    app = Application(consumer_group="visitors-15min2", use_changelog_topics=False)
+    app = Application(consumer_group="visitors-15min"+str(uuid.uuid4()), use_changelog_topics=False, auto_offset_reset='earliest')
 
     # Define the topic using the "output" environment variable
     input_topic_name = os.getenv("input", "")
@@ -26,30 +27,40 @@ def main():
     # Create a StreamingDataFrame to process inbound data
     sdf = app.dataframe(input_topic)
 
+    def initializer(row: dict):
+        return {
+            'user_ids': []
+        }
+
     # this funciton will be called for every row received
-    # it adds the userId from the row to state
     def row_processor(state: dict, row: dict):
-        state[row['userId']] = 1
+        
+        user_ids = state['user_ids'] # get user_ids array from state
+        userId = row['userId']
+        if userId not in user_ids: # if the userId has not already been seen in this window
+            user_ids.append(userId) # add it to the list of ids seen
+            state['user_ids'] = user_ids # update state
         return state
 
-    # create a 15 minute hopping window with a 10 second step
-    # pass each row to the row_processor function
-    # take the final result of the window
-    sdf = sdf.hopping_window(timedelta(minutes=15), timedelta(seconds=10)).reduce(row_processor, lambda r: row_processor({}, r)).current()
-    # data produced by hopping window is: {'start': 1710861638000, 'end': 1710862538000, 'value': {'ABC123': 1}}
-
-    # now we overwrite the 'value' column with the count of 
-    # the length of the value dictionary. This gives us the 
-    # number of unique visitors in the last 15 minutes
-    # we will get an update every 1 second
-    sdf["value"] = sdf["value"].apply(lambda u: len(u))
-    # data after this stage is: {'start': 1710861639000, 'end': 1710862539000, 'value': 15}
+    # create a 15 minute hopping window with a 60 second step
+    sdf = sdf.hopping_window(timedelta(minutes=15), timedelta(seconds=60)).reduce(row_processor, initializer).current()
+    
+    # combine the length of the user_ids list in state and the timestamp representing the start of the window
+    sdf = sdf.apply(lambda row: {"timestamp": row['start'], "count": len(row['value']['user_ids'])})
     
     # print data after any stage of the pipeline to see what you're working with
     sdf = sdf.update(lambda row: print(row))
 
+    # output rows:
+    # {'timestamp': 1712920080000, 'count': 2}
+    # {'timestamp': 1712920140000, 'count': 0}
+    # {'timestamp': 1712919300000, 'count': 3}
+    # {'timestamp': 1712919360000, 'count': 3}
+    # {'timestamp': 1712919420000, 'count': 3}
+    # {'timestamp': 1712919480000, 'count': 3}
+
     # publish the data to a topic
-    sdf = sdf.to_topic(output_topic)    
+    sdf = sdf.to_topic(output_topic)
 
     # run the pipeline defined above
     app.run(sdf)
