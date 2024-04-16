29ee895ca1709d004f3d67f8b1fc6fd492ae362c
Steve Rosam <srosam+quix-demos@gmail.com>
2024-04-16 12:47:59 +0000

    Edited 'main.py'

--- test reader/main.py
+++ test reader/main.py
@@ -4,22 +4,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# you decide what happens here!
-def sink(message):
-    print(message)
-    # write_to_db(value) # implement your logic to write data or send alerts etc
-
-    # for more help using QuixStreams see the docs:
-    # https://quix.io/docs/quix-streams/introduction.html
-
 app = Application.Quix("destination-v1", auto_offset_reset = "latest")
 
 input_topic = app.topic(os.environ["input"])
 
 sdf = app.dataframe(input_topic)
-
-# call the sink function for every message received.
-sdf = sdf.update(sink)
 
 # you can print the data row if you want to see what's going on.
 sdf = sdf.update(lambda row: print(row))
